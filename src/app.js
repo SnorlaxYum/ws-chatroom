8bfbe9ae3044b3c86a6f7a9165cf6d0b45cf5b88
Sim <sim@snorl.ax>
2021-08-31 23:04:52 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { exec } = require('child_process')
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+  app.whenReady().then(() => {
+    createWindow()
+    exec(`yarn vue serve`)
+  
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
