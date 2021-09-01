de6adb53e6017ea132f661ef876105e15923f15f
Sim <sim@snorl.ax>
2021-09-01 23:14:58 +0800

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,43 +1,50 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const { exec } = require('child_process')
+const { get } = require('http')
 
 function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
-  
-    win.loadFile('index.html')
-  }
-
-  app.whenReady().then(() => {
-    createWindow()
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
+
+  return win
+}
+
+function sleep(t) {
+  return new Promise(res => setTimeout(res, t))
+}
 
   app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
   })
 
-  app.whenReady().then(() => {
-    createWindow()
-    exec(`yarn vue serve`)
-  
-    app.on('activate', function () {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
+  app.whenReady().then(async() => {
+    const win = createWindow()
+    let accessible = false
+    exec(`yarn serve`)
 
-  window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
+    while(!accessible) {
+      get('http://localhost:8080/rtc', (res) => {
+        accessible = true
+      }).on('error', (e) => {
+        // console.error('error:', e)
+        win.loadFile('loading.html')
+      })
+      
+      await sleep(1000)
     }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
+
+    win.loadURL('http://localhost:8080/rtc')
+
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+        win.loadURL('http://localhost:8080')
+      }
+    })
   })
