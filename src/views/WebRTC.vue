3b7820ba30fe741dd0ba86f45090a5a4f4860986
Sim <sim@snorl.ax>
2021-09-05 21:28:52 +0800

    screen sharing and video chat

--- src/views/WebRTC.vue
+++ src/views/WebRTC.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="chatroom">
-    房间地址：<input class="roomUrl" readonly="true" v-model="roomUrl" /><br />
-    <video width="640px" height="480px" id="myVideo"></video>
-    <video width="240px" height="160px" id="remoteVideo"></video>
+    Room: <input class="roomUrl" readonly="true" v-model="roomUrl" /><br />
+    <div class='error' ref='roomError'></div>
+    <div class='camera'>
+      Camera <button @click="toggleVideo">Click to toggle both videos</button>
+      <div ref='videoPlace'>
+        <div id="myVideoWrapper" ref="myVideoWrapper">
+          <video id="myVideo" ref="myVideo"></video>
+        </div>
+        <div id="remoteVideoWrapper" style="opacity: 0;" ref="remoteVideoWrapper"><video id="remoteVideo" ref="remoteVideo"></video></div>
+      </div>
+    </div>
+    <div class='screen'>
+      Screen Share: <button @click="toggleVideo1">Click to toggle both videos</button>
+      <div ref='videoPlace1'>
+        <div id="myVideoWrapper1" ref="myVideoWrapper1">
+          <video id="myVideo1" ref="myVideo1"></video>
+        </div>
+        <div id="remoteVideoWrapper1" style="opacity: 0;" ref="remoteVideoWrapper1"><video id="remoteVideo1" ref="remoteVideo1"></video></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,7 @@
 // @ is an alias to /src
 let socket;
 let pc;
+let pc1;
 
 export default {
   name: "userMedia",
@@ -17,6 +35,8 @@ export default {
     return {
       roomUrl: "",
       roomName: "",
+      myVideoIndex: 1,
+      myVideoIndex1: 1
     };
   },
   async mounted() {
@@ -34,6 +54,24 @@ export default {
     });
   },
   methods: {
+    toggleVideo() {
+      if(this.myVideoIndex) {
+        this.$refs["videoPlace"].appendChild(this.$refs["myVideoWrapper"])
+        this.myVideoIndex = 0
+      } else {
+        this.$refs["videoPlace"].appendChild(this.$refs["remoteVideoWrapper"])
+        this.myVideoIndex = 1
+      }
+    },
+    toggleVideo1() {
+      if(this.myVideoIndex1) {
+        this.$refs["videoPlace1"].appendChild(this.$refs["myVideoWrapper1"])
+        this.myVideoIndex1 = 0
+      } else {
+        this.$refs["videoPlace1"].appendChild(this.$refs["remoteVideoWrapper1"])
+        this.myVideoIndex1 = 1
+      }
+    },
     getUrlParam(name) {
       var reg = new RegExp("(\\?|&)" + name + "=([^&]*)(&|$)");
       var r = window.location.href.substr(1).match(reg);
@@ -41,15 +79,51 @@ export default {
       return null;
     },
     start() {
-      const socket = new WebSocket('ws://localhost:9001');
+      socket = new WebSocket('ws://localhost:9001');
+      let isOffer = false
       //socket = new ScaleDrone("OXo4HSBTCQ8ehrxI");
       socket.addEventListener("open", () => {
-        socket.send(JSON.stringify({roomName: this.roomName}))
+        socket.send(JSON.stringify({type: 'init', roomName: this.roomName}))
       });
+      const that = this
       socket.addEventListener('message', function (event) {
         console.log('Message from server ', event.data);
+        const message = JSON.parse(event.data)
+        if(message.type === 'roomFull') {
+          that.$refs['roomError'].textContent = 'The room is full, plz select another room!'
+        } else if(message.type === 'roomInfo') {
+            isOffer = message.clientSum > 1
+            that.startWebRTC(isOffer)
+        } else if(message.sdp) {
+            const {id, sdp} = message
+            const curPC = id === 'mediaStream' ? pc : pc1
+            curPC.setRemoteDescription(
+              new RTCSessionDescription(sdp),
+              () => {
+                console.log('sdp消息类型：', curPC.remoteDescription.type)
+                if(curPC.remoteDescription.type === "offer") {
+                  curPC.createAnswer()
+                    .then(answer => {
+                      curPC.setLocalDescription(
+                        answer,
+                        () => {
+                          that.sendMessage({id, sdp: curPC.localDescription})
+                        },
+                        that.onError
+                      )
+                    })
+                    .catch(that.onError)
+                }
+              },
+              this.onError
+            )
+        } else if(message.candidate) {
+          const {id, candidate} = message
+          console.log('condidate消息', message)
+          const curPC = id === 'mediaStream' ? pc : pc1
+          curPC.addIceCandidate(new RTCIceCandidate(candidate))
+        }
       });
-      this.startWebRTC(false)
     },
     startWebRTC(isOffer) {
       const iceServer = {
@@ -60,11 +134,12 @@ export default {
         ],
       };
       console.log("startWebRTC, connect to stun server");
+      // userMedia
       pc = new RTCPeerConnection(iceServer);
       pc.onicecandidate = (event) => {
         if (event.candidate) {
           console.log(">>>>onicecandidate");
-          this.sendMessage({ candidate: event.candidate });
+          this.sendMessage({ id: 'mediaStream', candidate: event.candidate });
         }
       };
       if (isOffer) {
@@ -75,7 +150,7 @@ export default {
               pc.setLocalDescription(
                 sdp,
                 () => {
-                  this.sendMessage({ sdp: pc.localDescription });
+                  this.sendMessage({ id: 'mediaStream', sdp: pc.localDescription });
                 },
                 this.onError
               );
@@ -83,17 +158,19 @@ export default {
             .catch(this.onError);
         };
       }
-      pc.onaddstream = function (evt) {
-        document.querySelector("#remoteVideo").srcObject = evt.stream;
-        let videoEle = document.querySelector("#myVideo");
-        videoEle.srcObject = evt.stream;
-        videoEle.play();
+
+      pc.onaddstream = (evt) => {
+        console.log('add stream:', evt)
+        const video = this.$refs["remoteVideo"]
+        video.srcObject = evt.stream
+        video.play()
+        this.$refs['remoteVideoWrapper'].style.opacity = 100
       };
 
       window.navigator.getUserMedia(
         { audio: true, video: true },
         (stream) => {
-          let videoEle = document.querySelector("#myVideo");
+          let videoEle = this.$refs["myVideo"]
           console.log("start local stream");
           videoEle.srcObject = stream;
           videoEle.play();
@@ -101,12 +178,66 @@ export default {
         },
         this.onError
       );
+
+      // screen sharing
+      const iceServer1 = {
+        iceServers: [
+          {
+            urls: "stun:stun1.l.google.com:19302",
+          },
+        ],
+      };
+      pc1 = new RTCPeerConnection(iceServer1);
+      pc1.onicecandidate = (event) => {
+        if (event.candidate) {
+          console.log(">>>>onicecandidate - screen sharin");
+          this.sendMessage({ id: 'screen', candidate: event.candidate });
+        }
+      };
+      if (isOffer) {
+        pc1.onnegotiationneeded = () => {
+          // 创建本地sdp描述 (Session Description Protocol)
+          pc1.createOffer()
+            .then((sdp) => {
+              pc1.setLocalDescription(
+                sdp,
+                () => {
+                  this.sendMessage({ id: 'screen', sdp: pc1.localDescription });
+                },
+                this.onError
+              );
+            })
+            .catch(this.onError);
+        };
+      }
+
+      pc1.onaddstream = (evt) => {
+        console.log('add stream:', evt)
+        const video = this.$refs["remoteVideo1"]
+        video.srcObject = evt.stream
+        video.play()
+        this.$refs['remoteVideoWrapper1'].style.opacity = 100
+      };
+
+      window.navigator.mediaDevices.getDisplayMedia(
+        { video: true }
+      ).then(
+        (stream) => {
+          let videoEle = this.$refs["myVideo1"]
+          console.log("start local stream screen sharing");
+          videoEle.srcObject = stream;
+          videoEle.play()
+          pc1.addStream(stream);
+        },
+        this.onError
+      );
     },
     sendMessage(message) {
-      socket.publish({
+      socket.send(JSON.stringify({
+        type: 'roomMessage',
         room: this.roomName,
-        message,
-      });
+        ...message,
+      }));
     },
     onError(error) {
       console.log(error);
@@ -125,11 +256,90 @@ export default {
     width: 500px;
   }
 
-  #remoteVideo {
+  .error {
+    color: red;
+  }
+
+  .camera>div {
+    position: relative;
+    div {
+      &:first-child {
+        position: relative;
+        video {
+          width: 640px;
+          height: 480px;
+        }
+      }
+      &:nth-child(2) {
+        position: absolute;
+        right: 10px;
+        top: 0;
+        z-index: 10;
+        video {
+          width: 240px;
+          height: 160px;
+        }
+      }
+    }
+  }
+
+  .screen>div {
+    position: relative;
+    div {
+      &:first-child {
+        position: relative;
+        video {
+          width: 640px;
+          height: 480px;
+        }
+      }
+      &:nth-child(2) {
+        position: absolute;
+        right: 10px;
+        top: 0;
+        z-index: 10;
+        video {
+          width: 240px;
+          height: 160px;
+        }
+      }
+    }
+  }
+
+  #myVideoWrapper::before {
+    content: "Me";
     position: absolute;
-    right: 10px;
-    top: 20px;
-    z-index: 10;
+    width: 99%;
+    color: #fff;
+    background: rgba(0,0,0,.5);
+    bottom: 4px;
+  }
+
+  #remoteVideoWrapper::before {
+    content: "The Other";
+    position: absolute;
+    width: 99%;
+    color: #fff;
+    background: rgba(0,0,0,.5);
+    bottom: 4px;
+  }
+
+  #myVideoWrapper1::before {
+    content: "Me";
+    position: absolute;
+    width: 99%;
+    color: #fff;
+    background: rgba(0,0,0,.5);
+    bottom: 4px;
+  }
+
+  #remoteVideoWrapper1::before {
+    content: "The Other";
+    position: absolute;
+    width: 99%;
+    color: #fff;
+    background: rgba(0,0,0,.5);
+    bottom: 4px;
   }
 }
 </style>
--- src/wsserver.js
+++ src/wsserver.js
@@ -5,30 +5,59 @@ const wss = new WebSocketServer({ port: 9001 });
 // since we are doing a simple demo, so we don't use a sql here.
 // we mimic the db using Maps
 const rooms = new Map()
+const userConnections = new Map()
 
 wss.on('connection', function connection(ws, req) {
-  console.log(req.socket)
+//   console.log(req.socket)
+  let room
+  const clientNow = req.headers["sec-websocket-key"]
+  userConnections.set(clientNow, ws)
   ws.on('message', function incoming(message) {
-    // console.log('received: %s', message);
+    console.log('received mesg from %s: %s', req.headers["sec-websocket-key"], message);
     const data = JSON.parse(message)
     if(data.type === 'init') {
-        const room = rooms.get(data.roomName)
+        room = rooms.get(data.roomName)
+        let clients
         if(room) {
-            console.log('yep, we have a room')
+            clients = room.get('clients')
+            if(clients.size === 2) {
+              ws.send(JSON.stringify({type: 'roomFull'}))
+            } else {
+              const clientData = {headers: req.headers, role: 'member'}
+              clients.set(clientNow, clientData)
+              console.log('yep, we have a room.', [...clients.keys()])
+              ws.send(JSON.stringify({type: 'roomInfo', clientSum: clients.size}))
+            }
         } else {
             console.log('created a new room:', data.roomName)
-            const roomNow = new Map(), clientNow = req.headers["sec-websocket-key"]
-            roomNow.set('clients', new Map())
-            const clientData = new Map()
-            clientData.set('headers', req.headers)
-            clientData.set('role', 'creator')
-            roomNow.get('clients').set(clientNow, clientData)
-            ws.send(JSON.stringify({type: 'inRoom'}))
+            room = new Map()
+            room.set('clients', new Map())
+            const clientData = {headers: req.headers, role: 'creator'}
+            room.get('clients').set(clientNow, clientData)
+            clients = [...room.get('clients').keys()]
+            rooms.set(data.roomName, room)
+            ws.send(JSON.stringify({type: 'roomInfo', clients}))
         }
+    } else if(data.type === "roomMessage") {
+      if(data.sdp||data.candidate) {
+        for(const [user, connection] of userConnections) {
+          if(user !== clientNow) {
+            connection.send(JSON.stringify(data))
+          }
+        }
+      }
     }
     // ws.send(`received ur message: ${JSON.stringify(message.toString())}`)
     // ws.send(`socket message: ${JSON.stringify(req.headers)}`)
   });
+  ws.on('close', () => {
+    userConnections.delete(clientNow)
+    if(room && room.get('clients')) {
+      room.get('clients').delete(clientNow)
+      console.log(clientNow, ' left')
+    }
+    
+  })
 
-  ws.send('connected');
+  ws.send(JSON.stringify({type: 'connected'}));
 });
